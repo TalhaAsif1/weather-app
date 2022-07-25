1d1aa978ca34caef7986d9bbf7a692c520aa214f
Talha Bin Asif <94634484+TalhaAsif1@users.noreply.github.com>
2022-07-25 18:08:54 +0500

    Add project files.

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "427fd6983d4b25da6e2f5f86bfc0243f"
+    }
+}
+module.exports=constants
